bbf8ad339e56d01b96b13e6448c3f2b3801edd6c
Atul <atulgaikarag9@gmail.com>
2021-06-29 23:18:27 +0530

    Added Welcome

---
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListProgram/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist.LinkedListProgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListProgram/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist.LinkedListProgram;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+}
