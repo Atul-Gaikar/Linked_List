19ef0abe48db09502e5c8f7f9b48cea49f7506fb
Atul <atulgaikarag9@gmail.com>
2021-06-29 23:41:34 +0530

    Welcome Linked List Problem

--- src/main/java/com/bridgelabz/linkedlist/LinkedListProgram/WelcomeLinkedListProblem.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListProgram/WelcomeLinkedListProblem.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.linkedlist.LinkedListProgram;
 
-public class Welcome {
+public class WelcomeLinkedListProblem {
+
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome to Linked List Problem ");
 	}
 }
