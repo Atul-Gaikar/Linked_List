39ae7766626e6107e027cd6fa35f15584bbae1e2
Atul <atulgaikarag9@gmail.com>
2021-06-29 23:57:44 +0530

    UC1

---
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListProgram/Node.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.linkedlist.LinkedListProgram;
+
+public class Node<K> {
+	private Node next;
+	private K key;
+
+	public Node(K key) {
+		this.key = null;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
